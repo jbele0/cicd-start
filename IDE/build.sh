1a25d11fe49d8eb5498e200668cdbf5fcfaa5b48
jbele0 <jb0elephant@gmail.com>
2024-11-25 14:43:01 +0900

    init

--- IDE/build.sh
+++ IDE/build.sh
@@ -7,11 +7,18 @@ docker buildx version
 docker buildx create --name mybuilder --use
 
 
+
+#docker hub 에 계정 가입 하기
 #login
 docker login --username jbele0
-
 
 
 # Create the builder
 #docker buildx build  --builder multi-builder --platform linux/amd64,linux/arm64  -t jbele0/cloud-cicd --push .
 docker buildx build  --platform linux/amd64,linux/arm64  -t jbele0/cloud-cicd --push .
+
+#빌드
+#docker build . -t [도커허브ID]/buildx-test
+
+#푸시
+#docker push [도커허브ID]/buildx-test
